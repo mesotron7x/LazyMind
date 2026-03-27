689419f3454bfe87ec1c8901aca87ba759ca8f75
孙敬华 <sunjinghua.vendor@mwcy34hy4xm.domain.sensetime.com>
2026-03-27 16:27:45 +0800

    汇总迁移文件

--- backend/core/migrations/20260317120000_add_datasets_tables_and_kb_id.down.sql
+++
@@ -1,7 +0,0 @@
--- 20260317120000_add_datasets_tables_and_kb_id
--- +migrate Down
-
-DROP TABLE IF EXISTS default_datasets CASCADE;
-DROP TABLE IF EXISTS dataset_members CASCADE;
-DROP TABLE IF EXISTS datasets CASCADE;
-
--- backend/core/migrations/20260320100000_add_uploaded_files_table.down.sql
+++
@@ -1,4 +0,0 @@
--- 20260320100000_add_uploaded_files_table
--- +migrate Down
-
-DROP TABLE IF EXISTS "uploaded_files";
